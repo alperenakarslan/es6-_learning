54b8fe8e51cd5a2a82e1a2e1b07c65290c248ce5
Alperen Akarslan <alperenakarslan@Alperens-Laptop.local>
2024-03-05 20:24:43 +0300

    Spread

---
+++ Spread/app.js
@@ -0,0 +1,22 @@
+// Spread operators
+
+// const langs = ["Flutter", "Dart", "Node.js"];
+
+// console.log(...langs);
+
+// const langs2 = ["Javascript", "C++",...langs];
+
+// console.log(langs2);
+
+// const numbers = [1,2,3,4,5,6,7,8,9];
+
+// const [a,b, ...numbers2] = numbers;
+
+// console.log(a,b);
+// console.log(numbers2);
+
+const addNumbers = (a,b,c) => console.log(a+b+c);
+
+const nums = [100,200,300];
+
+addNumbers(...nums);
